6936e51ee8dad1962c65b821291ec2c79446c981
Jesse Carrigan <jesse.m.carrigan@gmail.com>
2017-09-20 10:52:55 -0700

    Added categories and function to set category.

--- src/app/bookish.js
+++ src/app/bookish.js
@@ -3,8 +3,22 @@ angular.module('Bookish', [
 ])
 .controller('MainCtrl', function($scope) {
     $scope.books = [
-        { "id": 1, "title": "Introduction to Algorithms" },
-        { "id": 2, "title": "Learning the bash shell" },
-        { "id": 3, "title": "Designing Object-Oriented Software"}
+        { "id": 1, "title": "Introduction to Algorithms", "category": "Computer Science"},
+        { "id": 2, "title": "Learning the bash shell", "category": "Linux" },
+        { "id": 3, "title": "Designing Object-Oriented Software", "category": "Software Development"}
     ];
+
+    $scope.categories = [
+        { "id": 1, "category": "Computer Science"},
+        { "id": 2, "category": "Linux" },
+        { "id": 3, "category": "Software Development"}
+    ];
+
+    $scope.currentCategory = null;
+    
+    function setCurrentCategory(category) {
+        $scope.currentCategory = category;
+    }
+
+    $scope.setCurrentCategory = setCurrentCategory;
 }); 
